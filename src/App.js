13a16d2fe458fc54b8c22de7b9fe4f3f948c2427
Nikhiln5k <nikhilskumar823@gmail.com>
2024-01-08 12:49:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,19 +15,19 @@ function App() {
         
         <Row isPoster="true" title="NetflixOriginals" fetchURL={requests.fetchNetflixOriginals}/>
   
-        <Row isPoster="true" title="Trending Movies" fetchURL={requests.fetchTrending}/>
+        <Row title="Trending Movies" fetchURL={requests.fetchTrending}/>
   
-        <Row isPoster="true" title="Top Rated Movies" fetchURL={requests.fetchTopRated}/>
-  
-        <Row isPoster="true" title="Action Movies" fetchURL={requests.fetchActionMovies}/>
-  
-        <Row isPoster="true" title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
-  
-        <Row isPoster="true" title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
-  
-        <Row isPoster="true" title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
-  
-        <Row isPoster="true" title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
+        <Row title="Top Rated Movies" fetchURL={requests.fetchTopRated}/>
+
+        <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+
+        <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+
+        <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+
+        <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+        
+        <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
   
       </div>
     </div>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -21,11 +21,13 @@ function Banner({fetchURL}) {
      
   return (
     <div style={{height:'600px',backgroundImage:`url(${base_url}${movie?.backdrop_path})`,backgroundSize:'cover' ,backgroundAttachment:'fixed'}}>
-        <div className='banner-content'>
-            <h1>{movie?.name}</h1>
-            <button className='btn btn-danger me-3'>Play<i class="fa-solid fa-play"></i></button>
-            <button className='btn btn-outline-light'>More info<i class="fa-solid fa-caret-down"></i></button>
-            <h2>{movie?.overview?.slice(0,200)}....</h2>
+        <div style={{width:"100%",height:"600px",background: "linear-gradient(to top, rgba(0, 0, 0), transparent)"}}>
+            <div className='banner-content'>
+              <h1>{movie?.name}</h1>
+              <button className='btn btn-danger me-3'>Play<i class="fa-solid fa-play"></i></button>
+              <button className='btn btn-outline-light'>More info<i class="fa-solid fa-caret-down"></i></button>
+              <h2>{movie?.overview?.slice(0,200)}....</h2>
+            </div>
         </div>
     </div>
   )
